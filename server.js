401b7149331b1942dd8961512b51501ccd413be6
unknown <tommyconner96@gmail.com>
2021-03-16 01:08:23 -0400

    url

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const placesRouter = require("./routers/placesRouter")
 const googleApiRouter = require("./routers/googleApi")
 const cors = require("cors")
 const server = express()
+const url = process.env.FRONTEND_URL
 
 require('dotenv').config()
 server.use(helmet())
@@ -12,7 +13,7 @@ server.use(cookieParser())
 server.use(express.json())
 
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000')
+    res.header("Access-Control-Allow-Origin", url)
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie")
     res.header("Access-Control-Allow-Credentials", true)
@@ -20,7 +21,7 @@ server.use((req, res, next) => {
 })
 server.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: url,
 }))
 
 server.use("/places/", placesRouter)
